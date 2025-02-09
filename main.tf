24e9cd5d292fe51ccd719bbcd26405fc3e45a119
Emmanuel1316 <163353895+Emmanuel1316@users.noreply.github.com>
2025-02-09 11:09:36 -0600

    initial commit_emmanuel

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data "aws_ami" "app_ami" {
+  data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+  instance_type = var.instance_type
 
   tags = {
     Name = "HelloWorld"
